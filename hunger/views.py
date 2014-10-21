6ea827fa5b2c5983ee25b6123bd57739d3754218
cswanso2 <cswanso2@illinois.edu>
2014-10-21 17:52:03 -0500

    Fixed foodnutritionrestauranthashtable

--- hunger/views.py
+++ hunger/views.py
@@ -24,10 +24,10 @@ def hunger(request):
 	restaurants = list(Restaurant.objects.all())
 	restaurantFoodNutrition = {}
 	for restaurant in restaurants:
-		foods = Food.objects.filter(restaurant__name=restaurant.name)
+		foods = list(Food.objects.filter(restaurant__id=restaurant.id))
 		foodNutrition = {}
 		for food in foods:
 			nutrition = Nutrition.objects.get(food_id=food.id)
 			foodNutrition[food] = nutrition
-	restaurantFoodNutrition[restaurant] = foodNutrition
+		restaurantFoodNutrition[restaurant] = foodNutrition
 	return render_to_response('index.html', {'restaurantFoodNutrition': restaurantFoodNutrition})
