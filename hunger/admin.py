3f812e1879d28c07995c536486910ecbc1d97728
EdwardChen28 <yawenchen28@gmail.com>
2014-10-23 22:22:34 -0500

    add search bar and viewing layout

--- hunger/admin.py
+++ hunger/admin.py
@@ -6,7 +6,7 @@ class UserAdmin(admin.ModelAdmin):
     fieldsets = [ (None,  {'fields':['username']}),
                   ('Log In', {'fields':('email','password')}),
                 ]
-    list_display = ('username','email','password')
+    list_display = ('username','email')
     search_fields =['username']
 
 class FoodInline(admin.TabularInline):
@@ -29,14 +29,24 @@ class SocialNetworkingAdmin(admin.ModelAdmin):
     search_fields =['name']
 
 ################Nutrition maybe#############33
-
-
+class NutritionAdmin(admin.ModelAdmin):
+    fieldsets = [ (None, {'fields':['food']}),
+                  ('Info', {'fields':('fat','protein','sugar','carbs','calories')}),
+                  ]
+    list_display =('food','fat','protein','sugar','carbs','calories')
+    search_fields =['food']
     
+class FoodAdmin(admin.ModelAdmin):
+    fieldsets = [ (None, {'fields':['name']}),
+                  ('Info', {'fields':('restaurant','price','averageRating')}),
+                  ]
+    list_display =('name','restaurant', 'price', 'averageRating')
+    search_fields =['name']
 # Register your models here.
 admin.site.register(User,UserAdmin)
-admin.site.register(Food)
+admin.site.register(Food, FoodAdmin)
 admin.site.register(Restaurant,RestaurantAdmin)
 admin.site.register(SocialNetworking,SocialNetworkingAdmin)
-admin.site.register(Nutrition)
+admin.site.register(Nutrition,NutritionAdmin)
 admin.site.register(FoodRating)
 admin.site.register(Share)
